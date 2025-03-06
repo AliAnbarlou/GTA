43535556777dc8f1fc2f64aec29503c1c7f21c41
Mehrabios <mohamadhosin.anbarloo@gmail.com>
2025-03-06 09:40:59 +0330

    s

--- Static/index.js
+++ Static/index.js
@@ -1,136 +1,193 @@
 (() => {
-  const searchContainer = document.querySelector('.search-container');
-  const searchInput = document.querySelector('.search-form__input');
-  const suggestionsList = document.querySelector('.suggestions');
-  let controller = new AbortController();
-  let debounceTimer;
+  const ROUTER = {
+    '/': {
+      onRoute() {
+        let controller = new AbortController(),
+          debounceTimer;
 
-  function removeChildren(element) {
-    while (element.firstChild) {
-      element.removeChild(element.firstChild);
-    }
+        const searchContainer = document.querySelector('.search-container'),
+          searchForm = document.querySelector('.search-form'),
+          searchInput = document.querySelector('.search-form__input'),
+          suggestionsList = document.querySelector('.suggestions');
 
-    element.textContent = '';
+        function removeChildren(element) {
+          while (element.firstChild) {
+            element.removeChild(element.firstChild);
+          }
+
+          element.textContent = '';
+        }
+
+        async function fetchSuggestions(query) {
+          controller.abort();
+          controller = new AbortController();
+
+          try {
+            const response = await fetch(`/word/api/${query}`, {
+              signal: controller.signal,
+            });
+
+            if (!response.ok) {
+              throw new Error('خطا در دریافت داده');
+            }
+
+            return [query, (await response.json()).hits] || [query, []];
+          } catch (error) {
+            return [query, []];
+          }
+        }
+
+        function renderSuggestions([query, suggestions]) {
+          removeChildren(suggestionsList);
+
+          if (suggestions.length === 0) {
+            const suggestionsError = document.createElement('p');
+
+            suggestionsError.textContent = `هیچ گونه پیشنهادی با عنوان "${query}" یافت نشد.`;
+            suggestionsError.classList.add('suggestions__error');
+            suggestionsList.appendChild(suggestionsError);
+          }
+
+          suggestions.unshift({ title: query });
+
+          const fragment = document.createDocumentFragment();
+
+          suggestions.forEach(({ title }) => {
+            const suggestionItem = document.createElement('li'),
+              suggestionLink = document.createElement('a');
+
+            suggestionLink.textContent = title;
+            suggestionItem.dataset.content = title;
+            suggestionLink.href;
+            suggestionLink.classList.add('suggestions__link');
+            suggestionItem.classList.add('suggestions__item');
+            suggestionItem.appendChild(suggestionLink);
+            fragment.appendChild(suggestionItem);
+          });
+
+          suggestionsList.appendChild(fragment);
+        }
+
+        function handleBlur() {
+          searchContainer.classList.remove('showing-results');
+        }
+
+        function handleFocus() {
+          if (suggestionsList.childElementCount > 0) {
+            searchContainer.classList.add('showing-results');
+          }
+        }
+
+        function handleInput() {
+          clearTimeout(debounceTimer);
+
+          const inputValue = searchInput.value.trim();
+
+          if (inputValue.length <= 1) {
+            removeChildren(suggestionsList);
+            searchContainer.classList.remove('showing-results');
+            return;
+          }
+
+          const hasLoadingSppiner =
+            !!suggestionsList.querySelector('.suggestions__loading-spinner') ||
+            false;
+
+          if (!hasLoadingSppiner) {
+            removeChildren(suggestionsList);
+            const loadingSppiner = document.createElement('span');
+
+            loadingSppiner.classList.add('suggestions__loading-spinner');
+            suggestionsList.appendChild(loadingSppiner);
+          }
+
+          searchContainer.classList.add('showing-results');
+
+          debounceTimer = setTimeout(async () => {
+            const suggestions = await fetchSuggestions(inputValue);
+            renderSuggestions(suggestions);
+          }, 850);
+        }
+
+        function handleKeyDown({ key: pressedKeyboardKey }) {
+          if (
+            pressedKeyboardKey !== 'ArrowUp' &&
+            pressedKeyboardKey !== 'ArrowDown'
+          ) {
+            return;
+          }
+
+          const hasSuggestionsItem =
+            !!suggestionsList.querySelector('.suggestions__item') || false;
+
+          if (!hasSuggestionsItem) {
+            return;
+          }
+
+          const activeSuggestion = suggestionsList.querySelector(
+            '.suggestions__item.active'
+          );
+
+          if (!activeSuggestion) {
+            suggestionsList.firstChild.classList.add('active');
+            return;
+          }
+
+          activeSuggestion.classList.remove('active');
+
+          let nextActiveSuggestion;
+
+          if (pressedKeyboardKey === 'ArrowUp') {
+            nextActiveSuggestion =
+              activeSuggestion === suggestionsList.firstElementChild
+                ? suggestionsList.lastElementChild
+                : activeSuggestion.previousElementSibling;
+          }
+
+          if (pressedKeyboardKey === 'ArrowDown') {
+            nextActiveSuggestion =
+              activeSuggestion === suggestionsList.lastElementChild
+                ? suggestionsList.firstElementChild
+                : activeSuggestion.nextElementSibling;
+          }
+
+          nextActiveSuggestion.classList.add('active');
+          searchInput.value = nextActiveSuggestion.dataset.content;
+        }
+
+        async function handleSubmit(ev) {
+          ev.preventDefault();
+
+          const hasActiveSuggestion =
+              searchContainer.classList.contains('showing-results') &&
+              suggestionsList.querySelector('.suggestions__item.active'),
+            selectedSuggestion = hasActiveSuggestion
+              ? suggestionsList.querySelector('.suggestions__item.active')
+                  .dataset.content
+              : searchInput.value;
+        }
+
+        searchInput.addEventListener('blur', handleBlur);
+        searchInput.addEventListener('focus', handleFocus);
+        searchInput.addEventListener('input', handleInput);
+        searchForm.addEventListener('submit', handleSubmit);
+        document.addEventListener('keydown', handleKeyDown);
+      },
+      cleanUp() {},
+    },
+    '': {
+      onRoute() {},
+      clenaUp() {},
+    },
+    404: {
+      onRoute() {},
+      cleanUp() {},
+    },
+  };
+
+  function navigate(ev) {
+    console.log(location.pathname);
   }
 
-  async function fetchSuggestions(query) {
-    controller.abort();
-    controller = new AbortController();
-
-    try {
-      const response = await fetch(`/word/api/${query}`, {
-        signal: controller.signal,
-      });
-
-      if (!response.ok) {
-        throw new Error('خطا در دریافت داده');
-      }
-
-      return (await response.json()).hits || [];
-    } catch (error) {
-      if (error.name !== 'AbortError') {
-        console.error('Request failed:', error);
-      }
-
-      return [];
-    }
-  }
-
-  function renderSuggestions(suggestions) {
-    searchContainer.classList.add('showing-results');
-
-    removeChildren(suggestionsList);
-
-    const fragment = document.createDocumentFragment();
-
-    suggestions.forEach(({ title }) => {
-      const item = document.createElement('li'),
-        suggestionLink = document.createElement('a');
-
-      suggestionLink.textContent = title;
-      suggestionLink.href;
-      item.classList.add('suggestions__item');
-      item.appendChild(suggestionLink);
-      fragment.appendChild(item);
-    });
-
-    suggestionsList.appendChild(fragment);
-  }
-
-  function handleKeydown(ev) {
-    const pressedKeyboardKey = ev.key;
-
-    if (
-      pressedKeyboardKey !== 'ArrowUp' &&
-      pressedKeyboardKey !== 'ArrowDown'
-    ) {
-      return;
-    }
-
-    const suggestionListChildren = suggestionsList.children,
-      suggestionsListLength = suggestionListChildren.length;
-
-    if (suggestionsListLength === 0) {
-      return;
-    }
-
-    const currentActiveSuggestions = suggestionsList.querySelector(
-        'suggestions__item.active'
-      ),
-      isLastChildren =
-        !currentActiveSuggestions ||
-        currentActiveSuggestions ===
-          suggestionListChildren.item(suggestionsListLength - 1);
-
-    currentActiveSuggestions.classList.remove('active');
-
-    (isLastChildren
-      ? suggestionsList.children.item(0)
-      : currentActiveSuggestions.nextElementSibling
-    ).classList.add('active');
-  }
-
-  function handleInput() {
-    clearTimeout(debounceTimer);
-
-    const inputValue = searchInput.value.trim();
-
-    if (inputValue.length <= 1) {
-      removeChildren(suggestionsList);
-      searchContainer.classList.remove('showing-results');
-      return;
-    }
-
-    const hasLoadingSppiner =
-      !!suggestionsList.querySelector('.suggestions__loading-spinner') || false;
-
-    if (!hasLoadingSppiner) {
-      removeChildren(suggestionsList);
-      const loadingSppiner = document.createElement('span');
-
-      loadingSppiner.classList.add('suggestions__loading-spinner');
-      suggestionsList.appendChild(loadingSppiner);
-    }
-
-    searchContainer.classList.add('showing-results');
-
-    debounceTimer = setTimeout(async () => {
-      const suggestions = await fetchSuggestions(inputValue);
-      renderSuggestions(suggestions);
-    }, 850);
-  }
-
-  function handleBlur() {
-    searchContainer.classList.remove('showing-results');
-  }
-
-  function handleFocus() {
-    if (suggestionsList.children.length > 0) {
-      searchContainer.classList.add('showing-results');
-    }
-
-    document.addEventListener('keydown', handleKeydown);
-  }
-
-  searchInput.addEventListener('input', handleInput);
+  window.addEventListener('load', navigate);
 })();
