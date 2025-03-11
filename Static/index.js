c873e4ab376a1003e736905a7a8d23f113313d0e
Mehrabios <mohamadhosin.anbarloo@gmail.com>
2025-03-11 22:52:40 +0330

    Initialaztion

--- Static/index.js
+++ Static/index.js
@@ -1,4 +1,6 @@
 (() => {
+  let currentRoute = '';
+
   const rootElement = document.querySelector('#root');
 
   async function fetchData(url = '.', options) {
@@ -61,193 +63,57 @@
     element.textContent = '';
   }
 
+  // const router = {
+
+  //   '/': {
+  //     regexPattern: /^\/$/,
+  //     get parentRoot() {
+  //       return document.querySelector('#root');
+  //     },
+  //     onParentRootLoad() {}
+  //   },
+  //   '/search': {
+  //     regexPattern: /^\/search\/?$/,
+  //     get parentRoot() {
+  //       return document.querySelector('#root');
+  //     },
+  //     HANDLE_TEXTAREA({ target: textAreaElement }) {
+  //       textAreaElement.style.height = 'auto';
+  //       textAreaElement.style.height = `${textAreaElement.scrollHeight}px`;
+  //     },
+  //     onRouteLoad() {
+  //       __CURRENT_WORD__ = new URLSearchParams(location.search).get('q');
+
+  //       const textAreas = document.querySelectorAll('.textarea');
+
+  //       textAreas.forEach((textArea) => {
+  //         textArea.addEventListener('input', this.HANDLE_TEXTAREA);
+  //       });
+  //     },
+  //   },
+  //   404: {},
+  //   onRootLayoutLoad() {
+
+  //   },
+  // };
+
   const router = {
-    '/': {
-      regexPattern: /^\/$/,
-    },
-    '/search': {
-      regexPattern: /^\/search\/?$/,
-      HANDLE_TEXTAREA({ target: textAreaElement }) {
-        textAreaElement.style.height = 'auto';
-        textAreaElement.style.height = `${textAreaElement.scrollHeight}px`;
-      },
-      onRouteLoad() {
-        __CURRENT_WORD__ = new URLSearchParams(location.search).get('q');
-
-        const textAreas = document.querySelectorAll('.textarea');
-
-        textAreas.forEach((textArea) => {
-          textArea.addEventListener('input', this.HANDLE_TEXTAREA);
-        });
-      },
-    },
-    404: {},
+    type: 'layout',
     onLoad() {
-      let controller = new AbortController(),
-        debounceTimer;
-
-      const searchContainer = document.querySelector('.search-container'),
-        searchForm = document.querySelector('.search-form'),
-        searchInput = document.querySelector('.search-form__input'),
-        suggestionsList = document.querySelector('.suggestions');
-
-      async function fetchSuggestions(query) {
-        controller.abort();
-        controller = new AbortController();
-
-        try {
-          const response = await fetch(`/word/api/${query}`, {
-            signal: controller.signal,
-          });
-
-          if (!response.ok) {
-            throw new Error('خطا در دریافت داده');
-          }
-
-          return [query, (await response.json()).hits] || [query, []];
-        } catch (error) {
-          return [query, []];
-        }
-      }
-
-      function renderSuggestions([query, suggestions]) {
-        removeChildren(suggestionsList);
-
-        suggestions = suggestions.filter((suggestion) =>
-          /^(?:[\u0600-\u06FF\s]+|[a-zA-Z\s]+)$/.test(suggestion.title)
-        );
-
-        if (suggestions.length === 0) {
-          const suggestionsError = document.createElement('p');
-
-          suggestionsError.textContent = `هیچ گونه پیشنهادی با عنوان "${query}" یافت نشد.`;
-          suggestionsError.classList.add('suggestions__error');
-          suggestionsList.appendChild(suggestionsError);
-          return;
-        }
-
-        suggestions.unshift({ title: query });
-
-        const fragment = document.createDocumentFragment();
-
-        suggestions.forEach(({ title }) => {
-          const suggestionItem = document.createElement('li'),
-            suggestionLink = document.createElement('a');
-
-          suggestionLink.textContent = title;
-          suggestionItem.dataset.content = title;
-          suggestionLink.dataset.spaLink = true;
-          suggestionLink.href = `/search/?q=${title}`;
-          suggestionLink.classList.add('suggestions__link');
-          suggestionItem.classList.add('suggestions__item');
-          suggestionItem.appendChild(suggestionLink);
-          fragment.appendChild(suggestionItem);
-        });
-
-        suggestionsList.appendChild(fragment);
-      }
-
-      function handleBlur() {
-        searchContainer.classList.remove('showing-results');
-      }
-
-      function handleFocus() {
-        if (suggestionsList.childElementCount > 0) {
-          searchContainer.classList.add('showing-results');
-        }
-      }
-
-      function handleInput() {
-        clearTimeout(debounceTimer);
-
-        const inputValue = searchInput.value.trim();
-
-        if (inputValue.length <= 1) {
-          removeChildren(suggestionsList);
-          searchContainer.classList.remove('showing-results');
-          return;
-        }
-
-        const hasLoadingSppiner =
-          !!suggestionsList.querySelector('.suggestions__loading-spinner') ||
-          false;
-
-        if (!hasLoadingSppiner) {
-          removeChildren(suggestionsList);
-          const loadingSppiner = document.createElement('span');
-
-          loadingSppiner.classList.add('suggestions__loading-spinner');
-          suggestionsList.appendChild(loadingSppiner);
-        }
-
-        searchContainer.classList.add('showing-results');
-
-        debounceTimer = setTimeout(async () => {
-          const suggestions = await fetchSuggestions(inputValue);
-          renderSuggestions(suggestions);
-        }, 850);
-      }
-
-      function handleKeyDown({ key: pressedKeyboardKey }) {
-        if (
-          pressedKeyboardKey !== 'ArrowUp' &&
-          pressedKeyboardKey !== 'ArrowDown'
-        ) {
-          return;
-        }
-
-        const hasSuggestionsItem =
-          !!suggestionsList.querySelector('.suggestions__item') || false;
-
-        if (!hasSuggestionsItem) {
-          return;
-        }
-
-        const activeSuggestion = suggestionsList.querySelector(
-          '.suggestions__item.active'
-        );
-
-        if (!activeSuggestion) {
-          suggestionsList.firstChild.classList.add('active');
-          return;
-        }
-
-        activeSuggestion.classList.remove('active');
-
-        let nextActiveSuggestion;
-
-        if (pressedKeyboardKey === 'ArrowUp') {
-          nextActiveSuggestion =
-            activeSuggestion === suggestionsList.firstElementChild
-              ? suggestionsList.lastElementChild
-              : activeSuggestion.previousElementSibling;
-        }
-
-        if (pressedKeyboardKey === 'ArrowDown') {
-          nextActiveSuggestion =
-            activeSuggestion === suggestionsList.lastElementChild
-              ? suggestionsList.firstElementChild
-              : activeSuggestion.nextElementSibling;
-        }
-
-        nextActiveSuggestion.classList.add('active');
-        searchInput.value = nextActiveSuggestion.dataset.content;
-      }
-
-      async function handleSubmit(ev) {
-        ev.preventDefault();
-
-        const selectedSuggestion = searchInput.value;
-        navigate(`/search/?q=${selectedSuggestion}`);
-        searchContainer.classList.remove('showing-results');
-      }
-
-      searchInput.addEventListener('blur', handleBlur);
-      searchInput.addEventListener('focus', handleFocus);
-      searchInput.addEventListener('input', handleInput);
-      searchForm.addEventListener('submit', handleSubmit);
-      document.addEventListener('keydown', handleKeyDown);
+      const themeToggler = document.querySelector('.header__theme-toggler');
     },
+    children: [
+      {
+        type: 'layout',
+        onLoad() {},
+        children: [],
+      },
+      {
+        type: 'layout',
+        onLoad() {},
+        children: [],
+      },
+    ],
   };
 
   const routes = Object.keys(router).filter(
