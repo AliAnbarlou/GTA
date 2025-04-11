d6a413dad41cd68094bff13b9ab72bb63c3cf77e
Ali Anbarlou <anbarloo@protonmail.com>
2025-04-11 17:01:14 +0330

    basics

--- Home/admin.py
+++ Home/admin.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
-from .models import IPAddress
+from .models import *
 
 @admin.register(IPAddress)
 class IPAddressAdmin(admin.ModelAdmin):
     list_display = ("ipAddr", "last_seen")  # نمایش هر دو فیلد در لیست
     ordering = ("-last_seen",)  # مرتب‌سازی بر اساس آخرین بازدید (جدیدترین‌ها بالا)
     search_fields = ("ipAddr",)  # امکان جستجو بر اساس IP
+admin.site.register(SocialMedia)
+admin.site.register(StaticPages)
+admin.site.register(ShowAds)
